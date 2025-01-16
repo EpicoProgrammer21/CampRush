c40f915558e4de2a3e41f217dfa120a3cc9d496c
EpicoProgrammer21 <danrichh210@gmail.com>
2025-01-16 22:00:44 +0700

    Feat: Add Comment

--- Assets/Script/VehicleSpawner.cs
+++ Assets/Script/VehicleSpawner.cs
@@ -6,27 +6,27 @@ public class VehicleSpawner : MonoBehaviour
 {
 
     [SerializeField]
-    private GameObject[] vehicleReference;
+    private GameObject[] vehicleReference; // Array untuk menyimpan referensi prefab kendaraan
 
     [SerializeField]
-    private int direction;
+    private int direction; // Arah gerakan kendaraan (positif atau negatif)
 
     [SerializeField]
-    private int[] speed = {7,5,6};
-    private GameObject spawnedVehicle;
+    private int[] speed = {7,5,6}; // Array untuk menyimpan kecepatan masing-masing jenis kendaraan
+    private GameObject spawnedVehicle;  // Referensi untuk kendaraan yang baru saja di-spawn
 
-    private int randomIndex;
+    private int randomIndex; // Indeks acak untuk memilih kendaraan dan kecepatannya
 
     
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnVehicles());
+        StartCoroutine(SpawnVehicles()); // Memulai coroutine untuk memunculkan kendaraan secara berkala
     }
 
     
-    IEnumerator SpawnVehicles(){
+    IEnumerator SpawnVehicles(){    // Coroutine untuk memunculkan kendaraan secara terus-menerus
         while (!GameManager.instance.CharStatus){
             // Debug.Log("Player alive? :" + GameManager.instance.CharStatus);
             yield return new WaitForSeconds(Random.Range(1,5));
@@ -39,6 +39,7 @@ public class VehicleSpawner : MonoBehaviour
             // scale.x *= direction;
             // spawnedVehicle.transform.localScale = scale;
             
+            // Tetapkan kecepatan gerakan kendaraan berdasarkan arah dan kecepatan yang sudah ditentukan
             spawnedVehicle.GetComponent<Vehicle>().speed = - direction * speed[randomIndex];
         } // while loop
     }
